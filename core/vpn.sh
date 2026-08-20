8a4e564ad296f34a747593ae971135a76fbaf974
thesnet320-net <thesnet320@gmail.com>
2026-08-21 00:26:27 +0100

    🔧 Correction core/vpn.sh: Pointer SERVER_HOST vers thesnet320-net/THE_S-TUNNEL-PRO-

--- core/vpn.sh
+++ core/vpn.sh
@@ -6,7 +6,7 @@ export PORT_OVPN_TCP=1194
 export PORT_OVPN_UDP=2200
 export PORT_SQUID=3128
 export PORT_OHP=8000
-export SERVER_HOST="https://raw.githubusercontent.com/RootNexTPro/nexTPro-ScriptAll/main"
+export SERVER_HOST="https://raw.githubusercontent.com/thesnet320-net/THE_S-TUNNEL-PRO-/main"
 export DTC=$(ip -o -4 route show to default | awk '{print $5}')
 install_packages() {
 rm /home/vps/public_html/*.ovpn

5aaf7538565bba35f959a63a9252fb65470a5cab
thesnet320-net <thesnet320@gmail.com>
2026-08-21 00:25:14 +0100

    🔧 Correction core/websocket.sh: Pointer vers thesnet320-net/THE_S-TUNNEL-PRO-

--- core/websocket.sh
+++ core/websocket.sh
@@ -11,7 +11,7 @@ fi
 PROXY_JS="/usr/local/sbin/proxy3.js"
 if [[ ! -f "$PROXY_JS" ]]; then
 echo "[ERROR] $PROXY_JS not found. Downloading..."
-wget -q -O "$PROXY_JS" "https://raw.githubusercontent.com/RootNexTPro/nexTPro-ScriptAll/main/module/proxy3.js"
+wget -q -O "$PROXY_JS" "https://raw.githubusercontent.com/thesnet320-net/THE_S-TUNNEL-PRO-/main/module/proxy3.js"
 chmod 644 "$PROXY_JS"
 else
 echo "[INFO] PROXY_JS already installed."

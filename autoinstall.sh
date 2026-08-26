a6499c8644dd97af8cfeb269991812b52797630b
wilfriedekongolo320-coder <wilfriedekongolo320@gmail.com>
2026-08-26 23:51:05 +0100

    🔄 Remplacer SERVER_HOST: thesnet320-net → wilfriedekongolo320-coder

--- autoinstall.sh
+++ autoinstall.sh
@@ -27,8 +27,8 @@ echo "[+] Désactivation SSH/SFTP pendant l'installation..."
 systemctl stop ssh 2>/dev/null
 systemctl disable ssh 2>/dev/null
 
-# 4. Téléchargement du Lanceur Principal depuis THE_S-TUNNEL-PRO-
-SERVER_HOST="https://raw.githubusercontent.com/thesnet320-net/THE_S-TUNNEL-PRO-/main"
+# 4. Téléchargement du Lanceur Principal (MODIFIÉ - POINTE VERS VOTRE CLONE)
+SERVER_HOST="https://raw.githubusercontent.com/wilfriedekongolo320-coder/Clone-THE_S-TUNNEL-/main"
 echo "[+] Connexion au dépôt autonome Nexus..."
 wget -qO /root/nexus.sh "$SERVER_HOST/nexus.sh"
 

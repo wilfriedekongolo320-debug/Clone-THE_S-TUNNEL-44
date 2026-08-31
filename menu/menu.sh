4c2fb0922310f9e24f8f4415b8c87269ddf589e0
thesnet320-source <thesnet320@gmail.com>
2026-08-31 01:37:52 +0100

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-readonly SERVER_HOST="https://raw.githubusercontent.com/RootNexTPro/nexTPro-ScriptAll/main"
+readonly SERVER_HOST="https://github.com/thesnet320-source/THE_S-TUNNEL-PRO-.git"
 clear
 
 # ==============================================================================

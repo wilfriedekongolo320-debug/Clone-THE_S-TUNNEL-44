b8a3de799d6899c3c76f6dd5d304524aefeabb59
thesnet320-source <thesnet320@gmail.com>
2026-09-05 10:44:56 +0100

    Add bot module init file

---
+++ bot/__init__.py
@@ -0,0 +1,9 @@
+# Bot Telegram - Module Principal
+__version__ = "1.0.0"
+__author__ = "THE_S Team"
+
+import os
+import sys
+
+# Ajouter le répertoire courant au path Python
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
